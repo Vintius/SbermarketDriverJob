8c03770fa8ecbe6330a7de7e60a09bd0f4415081
Vintius <46298999+Vintius@users.noreply.github.com>
2020-04-10 19:56:57 +0300

    Add adaptives (950px)

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,1 @@
 import "./import/modules";
-// import Selectr from 'mobius1-selectr';
-// import Inputmask from "inputmask";
-//
-// new Selectr('#city-id', {
-//         searchable: false,
-//         width: 300
-// });
-//
-// var selector = document.getElementById("phone-id");
-//
-// var im = new Inputmask("+7 (999)999-99-99");
-// im.mask(selector);
-
