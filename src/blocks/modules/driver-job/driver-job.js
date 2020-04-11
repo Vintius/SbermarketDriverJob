9ac5ce2146aa37e7df9d8e65230fa800cc6536d1
Vintius <46298999+Vintius@users.noreply.github.com>
2020-04-11 17:47:01 +0300

    Fix scroll and focus

--- src/blocks/modules/driver-job/driver-job.js
+++ src/blocks/modules/driver-job/driver-job.js
@@ -4,5 +4,13 @@ let driverJob = new Vue ({
    el: '#driverJob',
    data: {
       ofertaOpened: false
+   },
+   methods: {
+      scrollFocus: function (scroll, focus) {
+         this.$nextTick(function () {
+            document.getElementById(scroll).scrollIntoView();
+            document.getElementById(focus).focus();
+         });
+      }
    }
 });
