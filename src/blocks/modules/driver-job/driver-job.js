2a3fd2303a31dc5418bbdc7d2ad157440fb5454f
Vintius <46298999+Vintius@users.noreply.github.com>
2020-04-13 14:10:06 +0300

    Fixes

--- src/blocks/modules/driver-job/driver-job.js
+++ src/blocks/modules/driver-job/driver-job.js
@@ -4,6 +4,8 @@ import Multiselect from 'vue-multiselect';
 import {mask} from 'vue-the-mask';
 import axios from 'axios';
 
+window.submitted = false;
+
 window.driver_job = new Vue ({
     el: '#driverJob',
     components: {
@@ -23,8 +25,7 @@ window.driver_job = new Vue ({
         ],
         city_value: {name: 'Москва', id: 1},
         country_value: [],
-        isTouched: false,
-        submitted: false
+        isTouched: false
     },
     methods: {
         scrollFocus: function (scroll, focus) {
@@ -32,20 +33,20 @@ window.driver_job = new Vue ({
             document.getElementById(scroll).scrollIntoView();
             document.getElementById(focus).focus();
          });
-        },
-        clearSuccess: function () {
-            if(this.submitted) {
-                // document.querySelectorAll('input[type=text], input[type=tel]')
-                //     .forEach(el=>el.value = '');
-               alert('Форма отправлена');
-            }
         }
+        // clearSuccess: function () {
+        //     if(this.submitted) {
+        //         // document.querySelectorAll('input[type=text], input[type=tel]')
+        //         //     .forEach(el=>el.value = '');
+        //        alert('Форма отправлена');
+        //     }
+        // }
         // onTouch: function () {
         //     this.isTouched = true
         // }
     },
     created: function () {
-      axios.get('https://sbermarket.ru/api/operational_zones?sort=name')
+      axios.get('https://cors-anywhere.herokuapp.com/https://sbermarket.ru/api/operational_zones?sort=name')
           .then(response => {
              this.cities = response.data.operational_zones;
           })
